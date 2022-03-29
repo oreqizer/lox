fd8a946700d199be553ee06f83c96d3c881c2039
oreqizer <oreqizer@gmail.com>
2022-03-29 20:43:16 +0200

    feat: main fn boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args();
+    if args.len() > 1 {
+        println!("Usage: Usage: rlox [script]");
+        std::process::exit(64);
+    }
+
+    if args.len() == 1 {
+        // run from file
+    }
+
+    // run REPL
 }
