9a08c741b2a008749cb6dd64e48ab0140bcc7795
oreqizer <oreqizer@gmail.com>
2022-04-04 17:29:33 +0200

    chore(error): calculate row and column from offset

--- src/error.rs
+++ src/error.rs
@@ -1,29 +1,29 @@
 pub struct Error {
     msg: String,
-    row: usize,
-    column: usize,
+    offset: usize,
 }
 
 impl Error {
-    pub fn new(msg: &str, row: usize, column: usize) -> Self {
+    pub fn new(msg: &str, offset: usize) -> Self {
         Self {
             msg: msg.to_string(),
-            row,
-            column,
+            offset,
         }
     }
 
     pub fn format(&self, src: &str) -> String {
-        let line = src.split("\n").nth(self.row - 1).unwrap_or("");
+        let (row, column) = self.calc_row_column(src);
 
-        let row = match self.row {
-            0..=9 => format!("    {}", self.row),
-            10..=99 => format!("   {}", self.row),
-            100..=999 => format!("  {}", self.row),
-            _ => format!(" {}", self.row),
+        let line = src.split("\n").nth(row - 1).unwrap_or("");
+
+        let row = match row {
+            0..=9 => format!("    {}", row),
+            10..=99 => format!("   {}", row),
+            100..=999 => format!("  {}", row),
+            _ => format!(" {}", row),
         };
 
-        let spaces = (0..self.column + row.len() - 1)
+        let spaces = (0..column + row.len() - 1)
             .into_iter()
             .map(|_| ' ')
             .collect::<String>();
@@ -35,6 +35,26 @@ impl Error {
             line = line,
             spaces = spaces
         )
+    }
+
+    fn calc_row_column(&self, src: &str) -> (usize, usize) {
+        let mut row = 1;
+        let mut column = 0;
+
+        for (i, c) in src.chars().enumerate() {
+            if c == '\n' {
+                row += 1;
+                column = 0;
+            } else {
+                column += 1;
+            }
+
+            if i == self.offset {
+                break;
+            }
+        }
+
+        (row, column)
     }
 }
 
@@ -58,9 +78,9 @@ mod tests {
 }
 "#;
 
-        let error = Error::new("Unknown identifier", 6, 14);
+        let error = Error::new("Unknown field", 74);
 
-        assert_eq!(&error.format(&src), "Error: Unknown identifier
+        assert_eq!(&error.format(&src), "Error: Unknown field
 
     6 |         this.lol = omg; // just a comment
                      ^--- Here.");
